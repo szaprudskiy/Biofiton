4e477bd4bd45bcfc8f958c377262cd0e5718f27c
OlegYakub <zeiro299@mail.ru>
2016-12-27 13:55:52 +0200

    27.12

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+/*$( document ).ready(function() {
+  if($('.search input').css('width', '200px')){
+		console.log('hello');
+		$('.search').css('backgroundColor', '#d3e8f6')
+	}else{
+		console.log('sad');
+		$('.search').css('backgroundColor', '#fff')
+	};
+});
+console.log(1)*/
+ $('.header__select').styler();
